7fd6bd4303f9b6b248b3a98558289dfa9668e5e9
Paolo Chiodi <chiodi84@gmail.com>
2014-10-23 11:13:46 +0200

    removed trailing whitespaces

--- script/schema.sql
+++ script/schema.sql
@@ -14,14 +14,14 @@ FLUSH PRIVILEGES;
 CREATE TABLE foo (id VARCHAR(36), p1 VARCHAR(255), p2 VARCHAR(255), p3 VARCHAR(255), seneca VARCHAR(125));
 
 CREATE TABLE moon_bar (
-  id VARCHAR(36), 
-  str VARCHAR(255), 
-  `int` INT, 
-  bol BOOLEAN, 
-  wen TIMESTAMP, 
-  mark VARCHAR(255), 
-  `dec` REAL, 
-  arr TEXT, 
+  id VARCHAR(36),
+  str VARCHAR(255),
+  `int` INT,
+  bol BOOLEAN,
+  wen TIMESTAMP,
+  mark VARCHAR(255),
+  `dec` REAL,
+  arr TEXT,
   obj TEXT,
   seneca VARCHAR(125));
 
