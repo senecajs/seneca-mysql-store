9aefaf51322f3c0bd4a3c7088a0421e0ada11290
Paolo Chiodi <chiodi84@gmail.com>
2014-10-23 12:50:19 +0200

    follow seneca-store-test convention for reporting

--- test/mysql.ext.test.js
+++ test/mysql.ext.test.js
@@ -7,7 +7,7 @@
 
 var seneca = require('seneca')
 var chai = require('chai');
-chai.Assertion.includeStack = true; 
+chai.Assertion.includeStack = true;
 var assert = chai.assert
 var eyes = require('eyes');
 var async = require('async');
@@ -26,20 +26,23 @@ var verify = function(cb,tests){
 exports.test = function(si, cb) {
   async.series({
     removeAll: function(cb) {
+      console.log('removeAll');
       var foo = si.make({name$:'foo'})
       foo.remove$( {all$:true}, verify(cb, function(err){
       }))
     },
     listEmpty: function(cb) {
+      console.log('listEmpty');
       var foo = si.make({name$:'foo'})
       foo.list$({}, verify(cb, function(res){
         assert.equal( 0, res.length)
       }))
     },
     insert2: function(cb) {
+      console.log('insert2');
       var foo = si.make({name$:'foo'})
       foo.p1 = 'v1'
-      
+
       foo.save$(foo, verify(cb, function(foo){
         assert.isNotNull(foo.id)
         assert.equal('v1',foo.p1)
@@ -47,23 +50,27 @@ exports.test = function(si, cb) {
       }))
     },
     list1: function(cb) {
+      console.log('list1');
       scratch.foo1.list$({}, verify(cb, function(res){
         assert.equal( 1, res.length)
       }))
     },
 
     list2: function(cb) {
+      console.log('list2');
       scratch.foo1.list$({id:scratch.foo1.id}, verify(cb, function(res){
         assert.equal( 1, res.length)
       }))
     },
     load1: function(cb) {
+      console.log('load1');
       scratch.foo1.load$({id:scratch.foo1.id}, verify(cb, function(res){
         assert.isNotNull(res.id)
       }))
     },
 
     update: function(cb) {
+      console.log('update');
       scratch.foo1.p1 = 'v2'
 
       scratch.foo1.save$(verify(cb, function(foo){
@@ -73,15 +80,17 @@ exports.test = function(si, cb) {
     },
 
     load2: function(cb) {
+      console.log('load2');
       scratch.foo1.load$({id:scratch.foo1.id}, verify(cb, function(res){
         assert.equal('v2',res.p1)
       }))
     },
 
     insertwithsafe: function(cb) {
+      console.log('insertwithsafe');
       var foo = si.make({name$:'foo'})
       foo.p1 = 'v3'
-      
+
       foo.save$(verify(cb, function(foo){
         assert.isNotNull(foo.id)
         assert.equal('v3',foo.p1)
@@ -90,26 +99,30 @@ exports.test = function(si, cb) {
     },
 
     list3: function(cb) {
+      console.log('list3');
       scratch.foo2.list$({id:scratch.foo2.id}, verify(cb, function(res){
         assert.equal( 1, res.length)
       }))
     },
 
     list4: function(cb) {
+      console.log('list4');
       scratch.foo2.list$({id:scratch.foo2.id, limit$:1}, verify(cb, function(res){
         assert.equal( 1, res.length)
       }))
     },
-    
+
     // test limit$
     listwithlimit: function(cb) {
+      console.log('listwithlimit');
       scratch.foo2.list$({limit$:1}, verify(cb, function(res){
         assert.equal( 1, res.length)
       }))
     },
-    
+
     // test sort$
     listwithsort1: function(cb) {
+      console.log('listwithsort1');
       scratch.foo2.list$({sort$:{'p1':-1}}, verify(cb, function(res){
         assert.equal( 2, res.length)
         assert.equal('v2',res[0].p1)
@@ -117,18 +130,21 @@ exports.test = function(si, cb) {
     },
 
     listwithsort2: function(cb) {
+      console.log('listwithsort2');
       scratch.foo2.list$({sort$:{'p1':1}}, verify(cb, function(res){
         assert.equal( 2, res.length)
         assert.equal('v3',res[0].p1)
       }))
     },
-    
+
     remove1: function(cb) {
+      console.log('remove1');
       scratch.foo2.remove$( {id:scratch.foo2.id}, verify(cb, function(err){
       }))
     },
 
     list5: function(cb) {
+      console.log('list5');
       var foo = si.make('foo')
       foo.list$({}, verify(cb, function(res){
         assert.equal( 1, res.length)
