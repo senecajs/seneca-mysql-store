d7e5c7b71286319b14b9280ed9dd1c3710873715
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-06-17 13:12:13 +0300

    Streamline test db configuration

---
+++ test/support/db/schema.sql
@@ -0,0 +1,36 @@
+/* Copyright (c) 2012 Mircea Alexandru */
+/* execute using mysql -u root < dbschema.sql */
+
+DROP DATABASE IF EXISTS senecatest;
+CREATE DATABASE senecatest;
+
+USE senecatest;
+
+/* Create user senecatest with harmless privilege so that DROP USER does not throw error on next line if user does not exist */
+GRANT USAGE ON *.* TO 'senecatest';
+DROP USER 'senecatest';
+CREATE USER 'senecatest' IDENTIFIED BY 'senecatest';
+GRANT ALL PRIVILEGES ON senecatest.* TO senecatest;
+FLUSH PRIVILEGES;
+
+CREATE TABLE foo (id VARCHAR(36), p1 VARCHAR(255), p2 VARCHAR(255), p3 VARCHAR(255), seneca VARCHAR(125));
+
+CREATE TABLE moon_bar (
+  id VARCHAR(36),
+  str VARCHAR(255),
+  `int` INT,
+  bol BOOLEAN,
+  wen TIMESTAMP,
+  mark VARCHAR(255),
+  `dec` REAL,
+  arr JSON,
+  obj JSON,
+  seneca VARCHAR(125));
+
+CREATE TABLE product (id VARCHAR(36), name VARCHAR(255), price INT);
+
+CREATE TABLE incremental (
+  id INT AUTO_INCREMENT,
+  p1 VARCHAR(255),
+  PRIMARY KEY (id)
+);
