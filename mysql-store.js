1510866b41fdca0a67f0afe8eb09301b40857bd4
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-06-25 05:09:32 +0300

    Do refactor

--- mysql-store.js
+++ mysql-store.js
@@ -10,7 +10,9 @@ var QueryBuilder = require('./query-builder')
 
 const RelationalStore = require('./lib/relational-util')
 const Q = require('./lib/qbuilder')
+
 const { intern } = require('./lib/intern')
+const { asyncmethod } = intern
 
 var Eraro = require('eraro')({
   package: 'mysql'
@@ -175,10 +177,10 @@ function mysql_store (options) {
       const ctx = { seneca, db: internals.connectionPool }
 
       if (intern.is_update(msg)) {
-        return do_update(msg, ctx)
+        return intern.do_update(msg, ctx)
       }
 
-      return do_create(msg, ctx)
+      return intern.do_create(msg, ctx)
     }),
 
     load: asyncmethod(async function (msg) {
@@ -186,37 +188,38 @@ function mysql_store (options) {
       const { qent, q } = msg
       const ctx = { seneca, db: internals.connectionPool }
 
+      const where = intern.where_of_q(q, ctx)
 
-      const ent_table = RelationalStore.tablename(qent)
-      const where = where_of_q(q, ctx)
-
-
-      const out = await selectents({
+      return intern.loadent({
         ent: qent,
         where,
         limit: 1,
         offset: 0 <= q.skip$ ? q.skip$ : null,
         order_by: q.sort$ || null
       }, ctx)
-
-
-      if (0 === out.length) {
-        return null
-      }
-
-      return out[0]
     }),
 
     list: asyncmethod(async function (msg) {
       const seneca = this
+      const { qent, q } = msg
       const ctx = { seneca, db: internals.connectionPool }
 
-      const sel_query = make_query(msg, ctx)
-      const rows = await intern.execquery(sel_query, ctx)
+      const nat_query = intern.is_native(msg)
 
-      const { qent } = msg
+      if (null != nat_query) {
+        const rows = await intern.execquery(nat_query, ctx)
+        return rows.map(row => RelationalStore.makeent(qent, row))
+      }
 
-      return rows.map(row => RelationalStore.makeent(qent, row))
+      const where = intern.where_of_q(q, ctx)
+
+      return intern.listents({
+        ent: qent,
+        where,
+        limit: 0 <= q.limit$ ? q.limit$ : null,
+        offset: 0 <= q.skip$ ? q.skip$ : null,
+        order_by: q.sort$ || null
+      }, ctx)
     }),
 
     remove: asyncmethod(async function (msg) {
@@ -225,20 +228,15 @@ function mysql_store (options) {
       const ctx = { seneca, db: internals.connectionPool }
 
       if (q.all$) {
-        return remove_all(msg, ctx)
+        return intern.remove_many(msg, ctx)
       }
 
-      return remove_one(msg, ctx)
+      return intern.remove_one(msg, ctx)
     }),
 
-    // Return the underlying native connection object
-    native: function (args, cb) {
-      Assert(args)
-      Assert(cb)
-      Assert(args.ent)
-
-      cb(null, internals.connectionPool)
-    }
+    native: asyncmethod(async function (_msg) {
+      return internals.connectionPool
+    })
   }
 
 
@@ -278,287 +276,6 @@ function mysql_store (options) {
   })
 
   return {name: store.name, tag: meta.tag}
-}
-
-
-function compact(obj) {
-  return Object.keys(obj)
-    .map(k => [k, obj[k]])
-    .filter(([, v]) => undefined !== v)
-    .reduce((acc, [k, v]) => {
-      acc[k] = v
-      return acc
-    }, {})
-}
-
-
-function asyncmethod(f) {
-  return function (msg, done) {
-    const seneca = this
-    const p = f.call(seneca, msg)
-
-    Assert('function' === typeof p.then &&
-      'function' === typeof p.catch,
-      'The function must be async, i.e. return a promise.')
-
-    return p
-      .then(result => done(null, result))
-      .catch(done)
-  }
-}
-
-
-async function remove_all(msg, ctx) {
-  const { seneca } = ctx
-  const { q, qent } = msg
-
-  const ent_table = RelationalStore.tablename(qent)
-
-  const rows = await intern.selectrows({
-    columns: ['id'],
-    from: ent_table,
-    where: seneca.util.clean(q),
-    limit: 0 <= q.limit$ ? q.limit$ : null,
-    offset: 0 <= q.skip$ ? q.skip$ : null,
-    order_by: q.sort$ || null
-  }, ctx)
-
-  await intern.deleterows({
-    from: ent_table,
-    where: {
-      id: rows.map(x => x.id)
-    }
-  }, ctx)
-
-  return
-}
-
-
-async function remove_one(msg, ctx) {
-  const { seneca } = ctx
-  const { q, qent } = msg
-
-  const ent_table = RelationalStore.tablename(qent)
-
-  const out = await selectents({
-    ent: qent,
-    where: seneca.util.clean(q),
-    limit: 1,
-    offset: 0 <= q.skip$ ? q.skip$ : null,
-    order_by: q.sort$ || null
-  }, ctx)
-
-
-  if (0 === out.length) {
-    return null
-  }
-
-  const del_ent = out[0]
-
-  await intern.deleterows({
-    from: ent_table,
-    where: {
-      id: del_ent.id
-    }
-  }, ctx)
-
-
-  if (q.load$) {
-    return del_ent
-  }
-
-
-  return
-}
-
-
-async function insertent (args, ctx) {
-  const { ent } = args
-
-  const ent_table = RelationalStore.tablename(ent)
-  const entp = RelationalStore.makeentp(ent)
-
-  await intern.insertrow({
-    into: ent_table,
-    values: compact(entp)
-  }, ctx)
-
-  const out = await selectents({
-    ent,
-    where: { id: ent.id }
-  }, ctx)
-
-  if (0 === out.length) {
-    return null
-  }
-
-  return out[0]
-}
-
-
-async function do_create(msg, ctx) {
-  const { ent } = msg
-  const { id: gen_id } = await intern.generateid(ctx)
-
-  const new_id = null == ent.id$
-    ? gen_id
-    : ent.id$
-
-  const new_ent = ent.clone$()
-  new_ent.id = new_id
-
-
-  const upsert_fields = intern.is_upsert(msg)
-
-  if (null != upsert_fields) {
-    return upsertent(upsert_fields, { ent: new_ent }, ctx)
-  }
-
-
-  return insertent({ ent: new_ent }, ctx)
-}
-
-
-async function do_update(msg, ctx) {
-  const { ent } = msg
-  const { id: ent_id } = ent
-
-
-  const ent_table = RelationalStore.tablename(ent)
-  const entp = RelationalStore.makeentp(ent)
-
-
-  const update = await intern.updaterows({
-    table: ent_table,
-    set: compact(entp),
-    where: { id: ent_id }
-  }, ctx)
-
-  const updated_anything = update.affectedRows > 0
-
-  if (!updated_anything) {
-    return insertent({ ent }, ctx)
-  }
-
-
-  const out = await selectents({
-    ent,
-    where: { id: ent.id }
-  }, ctx)
-
-  if (0 === out.length) {
-    return null
-  }
-
-  return out[0]
-}
-
-
-async function upsertent(upsert_fields, args, ctx) {
-  const { ent } = args
-
-  const entp = RelationalStore.makeentp(ent)
-  const ent_table = RelationalStore.tablename(ent)
-
-  return intern.transaction(async (trx) => {
-    const trx_ctx = { ...ctx, db: trx }
-
-    const update_q = upsert_fields
-      .filter(c => undefined !== entp[c])
-      .reduce((h, c) => {
-        h[c] = entp[c]
-        return h
-      }, {})
-
-
-    if (_.isEmpty(update_q)) {
-      return insertent({ ent }, trx_ctx)
-    }
-
-    const update_set = _.clone(entp); delete update_set.id
-
-    await intern.updaterows({
-      table: ent_table,
-      where: update_q,
-      set: update_set
-    }, trx_ctx)
-
-    // TODO: TODO:
-    //
-    const ins_sel_query = QueryBuilder.insertwherenotexistsstm(ent, update_q)
-    //
-    await intern.execquery({
-      sql: ins_sel_query.text,
-      bindings: ins_sel_query.values
-    }, trx_ctx)
-
-    // NOTE: Because MySQL does not support "RETURNING", we must fetch
-    // the entity in a separate trip to the db. We can fetch the entity
-    // by the query and not worry about duplicates - this is because
-    // the query is unique by definition, because upserts can only work
-    // for unique keys.
-    //
-    const out = await selectents({ ent, where: update_q }, trx_ctx)
-
-    if (0 === out.length) {
-      return null
-    }
-
-    return out[0]
-  }, ctx)
-}
-
-
-function where_of_q(q, ctx) {
-  if ('string' === typeof q || Array.isArray(q)) {
-    return { id: q }
-  }
-
-  const { seneca } = ctx
-
-  return seneca.util.clean(q)
-}
-
-
-function make_query(msg, ctx) {
-  const { qent, q } = msg
-
-  if ('string' === typeof q.native$) {
-    return q.native$
-  }
-
-  if (Array.isArray(q.native$)) {
-    Assert(0 < q.native$.length, 'q.native$.length')
-    const [sql, ...bindings] = q.native$
-
-    return { sql, bindings }
-  }
-
-  const ent_table = RelationalStore.tablename(qent)
-  const where = where_of_q(q, ctx)
-
-  return Q.selectstm({
-    columns: '*',
-    from: ent_table,
-    where,
-    limit: 0 <= q.limit$ ? q.limit$ : null,
-    offset: 0 <= q.skip$ ? q.skip$ : null,
-    order_by: q.sort$ || null
-  })
-}
-
-
-async function selectents(args, ctx) {
-  const { ent } = args
-  const from = RelationalStore.tablename(ent)
-
-  const sel_args = { ...args, from, columns: '*' }
-  delete sel_args.ent
-
-  const rows = await intern.selectrows(sel_args, ctx)
-  const out = rows.map(row => RelationalStore.makeent(ent, row))
-
-  return out
 }
 
 
